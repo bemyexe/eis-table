f811020dcf8a5604f973e89ba27b195ca639f769
maxim <trifonov.max@mail.ru>
2024-07-31 22:41:13 +0300

    feature(core): added rc-table

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,38 @@
+import Table from 'rc-table';
+
+import { useMeters } from '../api';
+import { Comlumns } from '../const/const';
+
+import './style.css';
+
 export const App = () => {
-  return <div>app</div>;
+  const { columns } = Comlumns();
+
+  const { data, isLoading, page, setPage } = useMeters();
+
+  if (isLoading) return <div>Loading</div>;
+
+  return (
+    <div className="app">
+      <Table
+        className="table"
+        columns={columns}
+        data={data}
+        scroll={{ y: 870 }}
+        footer={() => (
+          <div>
+            <button
+              onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
+            >
+              prev
+            </button>
+            {'              '} <p>{page}</p>
+            <button onClick={() => setPage((prevPage) => prevPage + 1)}>
+              next
+            </button>
+          </div>
+        )}
+      />
+    </div>
+  );
 };
