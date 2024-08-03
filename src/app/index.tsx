cc7e2755ffd13e854d2fd16720906c72120bf84d
maxim <trifonov.max@mail.ru>
2024-08-03 15:12:59 +0300

    feature(app): combined datas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import Table from 'rc-table';
 
-import { useMeters } from '../api';
+import { useAreas, useMeters } from '../api';
 import { Comlumns } from '../const/const';
 
 import './style.css';
@@ -8,16 +8,28 @@ import './style.css';
 export const App = () => {
   const { columns } = Comlumns();
 
-  const { data, isLoading, page, setPage } = useMeters();
-
+  const { meters, isLoading, page, setPage } = useMeters();
+  const { areasQueries } = useAreas(meters);
   if (isLoading) return <div>Loading</div>;
+
+  // const data = [...meters, ...areas];
+
+  const newArray = meters.map((meter) => {
+    const area =
+      areasQueries.success &&
+      areasQueries.data.find((a) => a.id === meter.area.id);
+    return {
+      ...meter,
+      area: area?.address,
+    };
+  });
 
   return (
     <div className="app">
       <Table
         className="table"
         columns={columns}
-        data={data}
+        data={newArray}
         scroll={{ y: 870 }}
         footer={() => (
           <div>
