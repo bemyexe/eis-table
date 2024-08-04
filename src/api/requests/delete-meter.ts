27481c82e58a9751ac16c13d15f98809e41bfce7
maxim <trifonov.max@mail.ru>
2024-08-04 11:31:53 +0300

    refactor(requests): changed props type

--- src/api/requests/delete-meter.ts
+++ src/api/requests/delete-meter.ts
@@ -1,4 +1,4 @@
-export const deleteMeter = (meterId: string) => {
+export const deleteMeter = (meterId: number) => {
   return fetch(`http://showroom.eis24.me/api/v4/test/meters/${meterId}/`, {
     method: 'DELETE',
   });
