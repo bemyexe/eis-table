4b79d58f04adcfbf28df65a7078f31cd8cc43d66
maxim <trifonov.max@mail.ru>
2024-08-05 12:32:43 +0300

    feature(types): added Meter, Area types[ ]

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,9 +1,40 @@
-export interface Counter {
-  id: number;
-  type: string;
-  date: number;
-  isAutomatic: string;
-  currentValue: number;
-  address: string;
+export interface Meter {
+  id: string;
+  _type: string[];
+  area: {
+    id: string;
+  };
+  is_automatic: boolean | null;
+  communication: string;
   description: string;
+  serial_number: string;
+  installation_date: string;
+  brand_name: string | null;
+  model_name: string | null;
+  initial_values: number[];
 }
+
+export type Meters = Meter[];
+
+interface AreaHouse {
+  address: string;
+  id: string;
+  fias_addrobjs: string[];
+}
+
+export interface Area {
+  id: string;
+  number: number;
+  str_number: string;
+  str_number_full: string;
+  house: AreaHouse;
+}
+
+export type Areas = Area[];
+
+export interface FormatArea {
+  id: string;
+  address: string;
+}
+
+export type FormatAreas = FormatArea[];
