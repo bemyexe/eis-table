475ea272fd14d89661b0f53dd9149fb39ac90d34
maxim <trifonov.max@mail.ru>
2024-08-06 13:47:23 +0300

    refactor(app): updated table rendering and data merging in app component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Table from 'rc-table';
 
-import { Area, Meter } from '../../@types';
+import { Area } from '../../@types';
 import { useMetersQuery } from '../api/hooks/';
 import { useAreasQuery } from '../api/hooks/use-areas-query';
+import { formatAreas } from '../helpers';
+import { mergeData } from '../helpers/merge-data';
 
 import { COLUMNS } from './components/columns';
 
@@ -14,44 +16,39 @@ const MAX_TABLE_HEIGHT = 870;
 export const App = () => {
   const [page, setPage] = useState(1);
 
-  const { meters, isLoading } = useMetersQuery(page);
+  const { meters = [], isLoading } = useMetersQuery(page);
   const { areas, isSuccessAreas } = useAreasQuery(meters);
 
-  // const areas1 = formatAreas(areas);
+  const formattedAreas = isSuccessAreas ? formatAreas(areas as Area[]) : [];
+
+  const rows = mergeData(meters, formattedAreas);
 
   if (isLoading) return <div>Loading</div>;
 
-  // const newArray = meters?.map((meter: Meter) => {
-  //   const area =
-  //     isSuccessAreas && areas1?.find((a: Area) => a.id === meter.area.id);
-
-  //   return {
-  //     ...meter,
-  //     area: area?.address,
-  //   };
-  // });
-
   return (
     <div className="app">
-      <Table
-        className="table"
-        columns={COLUMNS}
-        data={meters}
-        scroll={{ y: MAX_TABLE_HEIGHT }}
-        footer={() => (
-          <div>
-            <button
-              onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
-            >
-              prev
-            </button>
-            {'              '} <p>{page}</p>
-            <button onClick={() => setPage((prevPage) => prevPage + 1)}>
-              next
-            </button>
-          </div>
-        )}
-      />
+      <div className="wrapper">
+        <div className="title">Список счетчиков</div>
+        <Table
+          className="table"
+          columns={COLUMNS}
+          data={rows}
+          scroll={{ y: MAX_TABLE_HEIGHT }}
+          footer={() => (
+            <div>
+              <button
+                onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
+              >
+                prev
+              </button>
+              {'              '} <p>{page}</p>
+              <button onClick={() => setPage((prevPage) => prevPage + 1)}>
+                next
+              </button>
+            </div>
+          )}
+        />
+      </div>
     </div>
   );
 };
