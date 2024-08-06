82ce25454ec17308ca250e76dec4b4c7acc8b3a7
maxim <trifonov.max@mail.ru>
2024-08-06 21:09:34 +0300

    feature(icons): added svg icons

--- src/app/components/shared/icon/enum/icon-enum.ts
+++ src/app/components/shared/icon/enum/icon-enum.ts
@@ -2,6 +2,8 @@ export enum Icons {
   trash = 'trash',
   ColdWaterAreaMeter = 'hvs',
   HotWaterAreaMeter = 'gvs',
+  trashActive = 'trash-active',
+  trashDisable = 'trash-disable',
 }
 
 export type IconKey = keyof typeof Icons;
