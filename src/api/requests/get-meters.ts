c0e0044612ae545e52184834dbbf735eb9ec9239
maxim <trifonov.max@mail.ru>
2024-08-06 21:21:11 +0300

    fix(requests): fixed url

--- src/api/requests/get-meters.ts
+++ src/api/requests/get-meters.ts
@@ -3,7 +3,7 @@ import { Meter } from '../../../@types';
 export const getMeters = (page = 1, limit = 20): Promise<Meter[]> => {
   const offset = (page - 1) * limit;
   return fetch(
-    `http://showroom.eis24.me/api/v4/test/meters/?limit=${limit}&offset=${offset}`
+    `https://showroom.eis24.me/api/v4/test/meters/?limit=${limit}&offset=${offset}`
   )
     .then((res) => res.json())
     .then((res) => res.results);
