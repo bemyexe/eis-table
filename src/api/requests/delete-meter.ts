0a9f2756c4c5eeebcb2a7738434d569dd2ffb016
maxim <trifonov.max@mail.ru>
2024-08-07 09:51:53 +0300

    fix(requests): fixed url

--- src/api/requests/delete-meter.ts
+++ src/api/requests/delete-meter.ts
@@ -1,7 +1,7 @@
 import { Meter } from '../../../@types';
 
 export const deleteMeter = (meterId: Meter['id']) => {
-  return fetch(`https://showroom.eis24.me/api/v4/test/meters/${meterId}/`, {
+  return fetch(`http://showroom.eis24.me/api/v4/test/meters/${meterId}/`, {
     method: 'DELETE',
   });
 };
--- src/api/requests/get-areas.ts
+++ src/api/requests/get-areas.ts
@@ -1,7 +1,7 @@
 import { Area, Meter } from '../../../@types';
 
 export const getAreas = (meterId: Meter['id']): Promise<Area[]> => {
-  return fetch(`https://showroom.eis24.me/api/v4/test/areas/?id__in=${meterId}`)
+  return fetch(`http://showroom.eis24.me/api/v4/test/areas/?id__in=${meterId}`)
     .then((res) => res.json())
     .then((res) => res.results);
 };
